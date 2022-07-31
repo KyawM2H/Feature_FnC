179d002519d5eb1e7681221b9493a235288bef62
kio maung <2236htetkyaw@gmail.com>
2022-07-31 18:02:30 +0100

    added box-shadow to the display area

--- feature.js
+++ feature.js
@@ -1,1 +1,13 @@
 import quote_data from "./quote_data.json" assert { type: "json" };
+
+const qouteEl = document.getElementById("quotation");
+const sourceEl = document.getElementById("source");
+
+const renderQuotes = () => {
+  let ramdomQoute = Math.floor(Math.random() * quote_data.length);
+  qouteEl.innerHTML = quote_data[ramdomQoute].quote;
+  sourceEl.innerHTML = quote_data[ramdomQoute].source;
+  console.log(ramdomQoute);
+};
+
+setInterval(renderQuotes, 3000);
